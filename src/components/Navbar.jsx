6cc73b3adcb5b0c313533ee8b955dc9315174349
Sourav <souravsahu70047@gmail.com>
2023-09-03 17:53:54 +0530

    Navbar Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import {headerLogo} from "./../assets/images";
-import {hamburger} from "./../assets/icons"
+import {hamburger} from "./../assets/icons";
+import {navLinks} from "./../constants/index";
 
 const Navbar = () => {
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
         <nav className='flex justify-between items-center max-container'>
-            <a href='/'>
-                <img src={headerLogo}/>
+            <a href='/'> 
+                <img src={headerLogo} alt="LOGO" width="130" height="29" />
             </a>
             <ul className='flex-1 flex justify-center items-start gap-16 max-lg:hidden'>
-                <li></li>
+                {navLinks.map((item)=>(
+                  <li key={item.label}>
+                    <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                      {item.label}
+                    </a>
+                  </li>
+                ))}
             </ul>
+            <div>
+              <img className='max-lg:block hidden' src={hamburger} alt="hamburger" width={25} height={25} />
+            </div>
         </nav>
     </header>
   )
