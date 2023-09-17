f16f119b63da518726d02a1fd7541e8bb64540ee
Sourav Kumar Sahu <souravsahu70047@gmail.com>
2023-09-17 17:04:40 +0530

    Added Special Offer and Button css

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({label,iconURL}) => {
+const Button = ({label,iconURL,backgroundColor,textColor,borderColor}) => {
   return (
-    <button className='flex justify-center items-center rounded-full px-7 py-4 border font-montserrat text-lg leading-none text-white  border-coral-red bg-coral-red'>
+    <button className={`flex justify-center items-center rounded-full px-7 py-4 border font-montserrat text-lg leading-none ${backgroundColor?`${textColor} ${borderColor} ${backgroundColor }`:"text-white  border-coral-red bg-coral-red"}`}>
         {label}
         {iconURL
           &&
--- src/components/SpecialOffers.jsx
+++ src/components/SpecialOffers.jsx
@@ -1,5 +1,6 @@
 import Button from "./Button"
 import { offer } from '../assets/images'
+import {arrowRight} from "./../assets/icons"
 
 const SpecialOffers = () => {
   return (
@@ -8,12 +9,13 @@ const SpecialOffers = () => {
         <img src={offer} alt="offer" width={773} height={687} className='object-contain w-full' />
       </div>
       <div className='flex-1'>
-          <h2 className='font-palanquin text-4xl font-bold lg:max-w-lg'>We Provide You <span className='text-coral-red'>Super Quality</span> Shoes</h2>
+          <h2 className='font-palanquin text-4xl font-bold lg:max-w-lg'>We Provide You <span className='text-coral-red'>Special</span> Offer</h2>
           <p className='gap-16 mt-4 info-text lg:max-w-lg'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, expedita non voluptatum, reprehenderit dignissimos iure beatae nesciunt minima magni voluptates quaerat. Reprehenderit magni ea cupiditate necessitatibus ipsam nemo explicabo voluptates.</p>
 
           <p className='gap-16 mt-6 lg:max-w-lg info-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, assumenda.</p>
-          <div className='mt-11'>
-            <Button label="View Details"/>
+          <div className='mt-11 flex flex-wrap gap-4'>
+            <Button label="Shop Now" iconURL={arrowRight}/>
+            <Button label="Learn More" backgroundColor="bg-white" borderColor="border-slate-gray" textColor="text-slate-gray" />
           </div>
       </div>
     </section>
